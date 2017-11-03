0bf909fb960b09f277da5c0998a74008162dba19
Nick Gentile <nickjgentile@gmail.com>
2017-11-03 14:34:46 -0700

    adding views, updating css, making menu animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,31 @@
 var int = angular.module('int', ['ngRoute']);
 
+int.config(['$routeProvider', function($routeProvider) {
+    
+        $routeProvider
+        .when('/settings', {
+            templateUrl: './views/settings.html'
+        })
+        .when('/addticket', {
+            templateUrl: './views/addticket.html'
+        })
+        .when('/company', {
+            templateUrl: './views/company.html'
+        })
+        .when('/user', {
+            templateUrl: './views/user.html'
+        })
+        .when('/userid', {
+            templateUrl: './views/userid.html'
+        })
+        .when('/home', {
+            redirectTo: 'index.html'
+        })
+        .otherwise({
+            redirectTo: 'index.html'
+        })
+    }])
+
 
 int.controller('intController', ['$scope', '$http', function($scope, $http){
 
