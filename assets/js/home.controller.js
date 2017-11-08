09b62d37a0952d8960ec3ab41a1657d5fc7dfbe9
Nick Gentile <nickjgentile@gmail.com>
2017-11-08 15:19:38 -0800

    adding user modal functionality

--- assets/js/home.controller.js
+++ assets/js/home.controller.js
@@ -1,18 +1,50 @@
 
 (function () {
     const Intercom = require('intercom-client');
-    
+
     var client = new Intercom.Client({ token: 'dG9rOjVlMzA5ZWRmXzY5ZjVfNGFkNV9iYzQ1XzlkOWJlMjEzZDQ3YzoxOjA=' });
-    
+
     angular
         .module('int')
         .controller('HomeController', ['$scope', '$http', function ($scope, $http) {
 
             $scope.loading = true;
 
+            $scope.modalload = true;
+
+            $scope.modal = false;
+
+            $scope.imageplace = './assets/images/placeholder.png';
+
+            $scope.hideModal = () => {
+                var htmlclass = document.getElementsByTagName('html')[0];
+
+                $scope.modal = false;
+                htmlclass.style.overflow = "scroll";
+            }
+
             $scope.userExpand = function (uid) {
-                console.log(uid)
-            };
+                var htmlclass = document.getElementsByTagName('html')[0];
+
+                $scope.modal = true;
+                $scope.modalload = true;
+                $scope.contentReady = false;
+                htmlclass.style.overflow = "hidden"
+
+                client
+                    .users
+                    .find({ user_id: uid })
+                    .then(function (r) {
+                        $scope.selectedUser = r.body
+                        console.log($scope.selectedUser)
+                        $scope.modalload = false;
+                        $scope.contentReady = true;
+                        $scope.$apply();
+                    })
+                    .catch(function (err) {
+                        console.log(err)
+                    })
+            }
 
             client
                 .users
