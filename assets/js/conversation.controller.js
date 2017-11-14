174846aa188c143b6f597d851324470ca5194c01
Nick Gentile <nickjgentile@gmail.com>
2017-11-14 15:17:05 -0800

    updates to the conversation html

--- assets/js/conversation.controller.js
+++ assets/js/conversation.controller.js
@@ -11,13 +11,43 @@
 
             $scope.loadingConvs = false;
             $scope.allConvs = [];
+            $scope.userIDs = {
+                '5887d6d9b3366404b4915db1': 'Anthony'
+            };
             $scope.pages;
+            $scope.searchFilter = false;
+            $scope.timeFilter = false;
+
+            $scope.inputToggle = () => {
+                $scope.searchFilter = !$scope.searchFilter
+            }
+
+            $scope.dateToggle = () => {
+                $scope.timeFilter = !$scope.timeFilter
+            }
 
             $scope.getDate = (date) => {
                 var newDate = new Date();
                 newDate.setTime(date * 1000)
-                var dateString = newDate.toUTCString();
+                var currDay = newDate.getDate();
+                var currMonth = newDate.getMonth();
+                var currYear = newDate.getFullYear();
+                var dateString = currDay + '/' + currMonth + '/' + currYear
                 return dateString;
+            }
+
+            $scope.getName = (id) => {
+                console.log(id)
+                client
+                    .users
+                    .find({ id: id })
+                    .then(function (r) {
+                        console.log(r.body.name)
+                        return r.body.name;
+                    })
+                    .catch(function (err) {
+                        console.log(err);
+                    })
             }
 
             $scope.removeTags = (str) => {
@@ -53,7 +83,7 @@
                 console.log($scope.pages);
                 console.log(count);
 
-                for(let count = 1; count <= $scope.pages; count++) {         
+                for (let count = 1; count <= $scope.pages; count++) {
                     client
                         .conversations
                         .list({ per_page: 60, page: count })
